a21aa796b8246f5811c70bab1b936a631d507a41
Nikita Gupta <nikitagupta1709@gmail.com>
2023-01-02 18:50:30 +0530

    started

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express  from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(express.json()); 
+app.use(cors());
+
+app.listen(3050, ()=>{
+    try{
+        // connection();
+        console.log("Server running on 3050");
+    }
+    catch(e){
+        console.log(e)
+    }
+})
